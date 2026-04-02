ca474319e9e54e07e554b57e45fc925a9022bd31
Nina Huang <nmhuang@ucsb.edu>
2026-04-02 13:18:27 -0700

    NH - added in supabase authentication backend

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,7 +1,19 @@
 "use client";
+import { useEffect } from "react";
 import Login from "./login/page";
+import {supabase} from '../lib/supabaseClient';
 
 export default function Home() {
+  useEffect(() => {
+    const connection = async() => {
+      const {data, error} = await supabase.from('Profiles').select('*');
+      console.log("data:", data);
+      console.log("error: ", error);
+    };
+
+    connection();
+  }, []);
+
   return (
     <div>
       <Login/>
