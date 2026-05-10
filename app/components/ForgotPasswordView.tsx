fc4e0c73b033e352907cccfd0c570b3229d94c63
Nina Huang <nmhuang@ucsb.edu>
2026-05-10 14:26:41 -0700

    NH - added reset password email but still need to create the reset password page

--- app/components/ForgotPasswordView.tsx
+++ app/components/ForgotPasswordView.tsx
@@ -6,8 +6,34 @@ import Image from "next/image";
 import ArrowBack from '@mui/icons-material/ArrowBack';
 import Link from 'next/link';
 import Form from "next/form";
+import {useState} from "react";
+import {supabase} from "../lib/supabaseClient";
 
 export default function ForgotPasswordView() {
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState<string | null>(null);
+    const [error, setError] = useState<string | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleResetPassword = async () => {
+        if (!email) return;
+        setIsLoading(true);
+        setError(null);
+        setMessage(null);
+
+        const {error} = await supabase.auth.resetPasswordForEmail(email, {
+            redirectTo: `${window.location.origin}/resetpassword`,
+        });
+
+        if (error) {
+            setError("Something went wrong. Please try again.");
+        } else {
+            setMessage("Password reset link sent! Check your email.");
+        }
+
+        setIsLoading(false);
+    }
+
     return <div className={styles.pageContainer}>
         <Image 
             src="/assets/mumunote-logo-long.png"
@@ -42,11 +68,17 @@ export default function ForgotPasswordView() {
                     name="forgot-password-email"
                     required={true}
                     placeholder="Email address"
+                    value = {email}
+                    onChange={(e) => setEmail(e.target.value)}
                 />
+                {message && <p className={styles.successText}>{message}</p>}
+                {error && <p className={styles.errorText}>{error}</p>}
                 <FormButton
-                    value="Send reset link"
+                    value={isLoading ? "Sending...": "Send reset link"}
                     name="send-email-button"
                     form="forgot-password-form"
+                    onClick={handleResetPassword}
+                    disabled={isLoading}
                 />    
             </Form>
         </div>
