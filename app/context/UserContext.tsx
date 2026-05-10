826c8cd5ad4914949d605acd8f6e4f5913ead19e
Nina Huang <nmhuang@ucsb.edu>
2026-05-10 14:05:56 -0700

    NH - fixed bug added in loading state to user context

--- app/context/UserContext.tsx
+++ app/context/UserContext.tsx
@@ -4,26 +4,29 @@ import {createContext, useContext, useEffect, useState} from "react";
 import {supabase} from '../lib/supabaseClient';
 import {Session, User} from '@supabase/supabase-js';
 
-const UserContext = createContext<{user: User | null} | null>(null);
+const UserContext = createContext<{user: User | null, loading: boolean} | null>(null);
 
 export const UserProvider = ({children} : {children: React.ReactNode}) => {
     const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         // get initial session
         supabase.auth.getSession().then(({ data: { session } }) => {
             setUser(session?.user ?? null);
+            setLoading(false);
         });
 
         // listen for auth changes (login, logout, token refresh)
         const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session: Session | null) => {
             setUser(session?.user ?? null);
+            setLoading(false);
         });
 
         return () => subscription.unsubscribe();
     }, []);
 
-    return (<UserContext.Provider value = {{user}}>
+    return (<UserContext.Provider value = {{user, loading}}>
         {children}
     </UserContext.Provider>);
 };
